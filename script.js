37fcf4870a6a3155f1babb04ebc6d83d80c37b06
MarcusAureliusLinden <gottemaja@gmail.com>
2020-11-18 03:23:44 +0000

    just trying

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-$("#button").click(function(){
-    $("#button").text("Wander on");
-    $(".poem").text(`"Millie was a child 
-prone to both wonder
-and wandering
-one night after the sky
-cleared from clouds of thunder
-over her fell a malicious spell
-and as she was under
-she couldn’t tell
-the fiery pits of hell
-from her mother's warm hearth 
-and ran away into the dark"`)
+let poem = document.getElementById("poem");
+
+function changePage() {
+    button.text("Wander on");
+    alert("There is no turning back")
 })
+};
+
+/*
+let poemPage = [
+    text,
+]
+
 
 
 function Millie(wonder, clarity, earnest) {
     wonder = 0;
     clarity = 0;
     earnest = 0;
-}
-
-click
+}; */
